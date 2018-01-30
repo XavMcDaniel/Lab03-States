4e7864d7b6ac85ec2515a993f7beef4a3c60b3c7
Xavier McDaniel <S01051073@acad.tri-c.edu>
2018-01-30 14:04:19 -0500

    Finished Assignment

---
+++ Lab03-States/Lab03-States/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = "Virginia"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Label1.Text = "North Carolina"
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Label1.Text = "South Carolina"
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Label1.Text = "Georgia"
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Label1.Text = "Alabama"
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Label1.Text = "Florida"
+    End Sub
+End Class
